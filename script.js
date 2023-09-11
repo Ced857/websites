c7ef2e59ccf43b6a25543db0a6f67521d06c84a7
Ced857 <bentuzallouiseced@gmail.com>
2023-09-11 22:21:34 +0800

    Add files via upload
    first web site

---
+++ script.js
@@ -0,0 +1,12 @@
+// // script.js
+// window.addEventListener('scroll', function () {
+//     const col1 = document.querySelector('.col-1');
+//     const col1Position = col1.getBoundingClientRect().top;
+  
+//     // Adjust this value as needed based on when you want the animation to trigger
+//     const triggerPoint = window.innerHeight - 100;
+  
+//     if (col1Position < triggerPoint) {
+//       col1.classList.add('animate-enter');
+//     }
+//   });
